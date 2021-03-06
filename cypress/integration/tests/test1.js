8057fecd9f11a218521db07dc46a2b76e1530e7b
Anand <56091994+anandhaprakash-mani@users.noreply.github.com>
2021-03-05 22:16:38 -0600

    Updates to test1.js

--- cypress/integration/tests/test1.js
+++ cypress/integration/tests/test1.js
@@ -3,6 +3,13 @@ describe("My first test suite", () => {
     it("My first test case",()=>{
         cy.fixture("site-data.json").as("siteData").then((siteData)=> {
             cy.visit(siteData.testSite2);
+            cy.get('.ProductCardComponent .details__header--title').should("have.length", 20);
+            // cy.get('.ProductCardComponent .details__header--title').contains("Buildium").click();
+            cy.get('.ProductCardComponent .details__header--title').each((el) => {
+                if(el.text() === "Buildium"){
+                    el.click();
+                }
+            })
         });
     })
 })
